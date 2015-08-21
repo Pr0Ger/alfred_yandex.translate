1de0a3aa8a91fdaec2c9ded4f0a9622f5b9bb772
Sergey Petrov <git@pr0ger.org>
2015-08-21 12:17:22 +0300

    First version of this

---
+++ translate.py
@@ -0,0 +1,22 @@
+
+import sys
+from workflow import Workflow, ICON_WEB, web
+
+API_KEY = 'trnsl.1.1.20150814T095541Z.7d37d9cfc8192d15.daf236523ceddc759bbd2bf347426ec656e73391'
+
+def main(wf):
+    url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+    params = dict(key=API_KEY, lang='ru', text=wf.args[0])
+    r = web.get(url, params)
+
+    r.raise_for_status()
+
+    result = r.json()
+
+    wf.add_item(title=result['text'][0])
+
+    wf.send_feedback()
+
+if __name__ == u"__main__":
+    wf = Workflow()
+    sys.exit(wf.run(main))
