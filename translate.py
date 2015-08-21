f0ea8236399d90d0790ff9f12c678ccb8e21a752
Sergey Petrov <git@pr0ger.org>
2015-08-21 13:57:57 +0300

    Added translating to english

--- translate.py
+++ translate.py
@@ -6,6 +6,7 @@ API_KEY = 'trnsl.1.1.20150814T095541Z.7d37d9cfc8192d15.daf236523ceddc759bbd2bf34
 
 def main(wf):
     url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+    # RU
     params = dict(key=API_KEY, lang='ru', text=wf.args[0])
     r = web.get(url, params)
 
@@ -13,7 +14,17 @@ def main(wf):
 
     result = r.json()
 
-    wf.add_item(title=result['text'][0])
+    wf.add_item(title=result['text'][0], subtitle='-> RU')
+
+    # EN
+    params = dict(key=API_KEY, lang='en', text=wf.args[0])
+    r = web.get(url, params)
+
+    r.raise_for_status()
+
+    result = r.json()
+
+    wf.add_item(title=result['text'][0], subtitle='-> EN')
 
     wf.send_feedback()
 
